d9632368f654330493333f59fdd584645737537d
Roxanne Weber <marie.sankofa@gmail.com>
2021-11-03 13:08:49 -0500

    completed birthday project -- pulling data from file, iterating the data and pulling data length plus destructuring object key-value pairs and displaying on screen

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,17 +2,14 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
+	const [people, setPeople] = useState(data); // here is where we define 'data' as the value to use for our people variable
 	return (
 		<main>
 			<section className='container'>
-				<h3>0 birthdays today</h3>
-				<List />
-				<button
-					className='btn'
-					onClick={() => {
-						console.log('you clicked me');
-					}}
-				>
+				<h3>{people.length} birthdays today</h3>
+				<List people={people} />{' '}
+				{/*this sets up people to be equal to our people state value*/}
+				<button className='btn' onClick={() => setPeople([])}>
 					clear all
 				</button>
 			</section>
